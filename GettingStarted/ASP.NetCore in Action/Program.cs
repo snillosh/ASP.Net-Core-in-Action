6c1a3b18e4198c825fafe156cc3a533c483298bd
Bevan <bevansalter@googlemail.com>
2024-08-24 11:07:47 +0100

    Chapter 4 and the UseWelcomePage middleware

--- GettingStarted/ASP.NetCore in Action/Program.cs
+++ GettingStarted/ASP.NetCore in Action/Program.cs
@@ -1,22 +1,6 @@
-using Microsoft.AspNetCore.HttpLogging;
-using Microsoft.Extensions.Logging;
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+WebApplication app = builder.Build();
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddHttpLogging(opts => opts.LoggingFields = HttpLoggingFields.RequestProperties);
-
-builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseHttpLogging();
-}
-
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/person", () => new Person("Bevan", "Salter"));
+app.UseWelcomePage();
 
 app.Run();
-
-public record Person(string FirstName, string lastName);
