12692f6d5b582f03438a16ed47b6755430f43e2e
Bevan <bevansalter@googlemail.com>
2024-08-25 16:10:40 +0100

    Listing 6.2 Generating a URL LinkGenerator and a named endpoint

--- GettingStarted/ASP.NetCore in Action/Program.cs
+++ GettingStarted/ASP.NetCore in Action/Program.cs
@@ -5,8 +5,14 @@ builder.Services.AddRazorPages();
 
 WebApplication app = builder.Build();
 
-app.MapGet("/test", () => "Hello world!");
-app.MapHealthChecks("/healthz");
-app.MapRazorPages();
+app.MapGet("/product/{name}", (string name) => $"The product is {name}")
+    .WithName("product");
+
+app.MapGet("/links", (LinkGenerator links) =>
+{
+    string link = links.GetPathByName("product",
+        new { name = "big-widget" });
+    return $"View the product at {link}";
+});
 
 app.Run();
