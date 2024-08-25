e74e655646aee743235ec7e83c8e02d940b40b76
Bevan <bevansalter@googlemail.com>
2024-08-25 11:41:19 +0100

    Adding multiple filters to the endpoint filter pipeline

--- GettingStarted/ASP.NetCore in Action/Program.cs
+++ GettingStarted/ASP.NetCore in Action/Program.cs
@@ -8,7 +8,14 @@ app.MapGet("/fruit/{id}", (string id) =>
     _fruit.TryGetValue(id, out var fruit)
         ? TypedResults.Ok(fruit)
         : Results.Problem(statusCode: 404))
-    .AddEndpointFilter(ValidationHelper.ValidateId);
+    .AddEndpointFilter(ValidationHelper.ValidateId)
+    .AddEndpointFilter(async (context, next) =>
+    {
+    app.Logger.LogInformation("Executing filter...");
+        object? result = await next(context);
+        app.Logger.LogInformation($"Handler result: {result}");
+        return result;
+    });
 
 app.Run();
 
