b6b2c63f23a5c54d4a9b88a3933c2cac3fd51181
Bevan <bevansalter@googlemail.com>
2024-08-25 10:33:23 +0100

    A minimal API that uses a value from the URL

--- GettingStarted/ASP.NetCore in Action/Program.cs
+++ GettingStarted/ASP.NetCore in Action/Program.cs
@@ -1,9 +1,24 @@
+using System.Xml.Linq;
+using System;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 
-app.UseStaticFiles();
-app.UseRouting();
+var people = new List<Person>
+{
+    new("Tom", "Hanks"),
+    new("Denzel", "Washington"),
+    new("Leondardo", "DiCaprio"),
+    new("Al", "Pacino"),
+    new("Morgan", "Freeman"),
+    new("Tom", "Killer"),
+    new("Tim", "Copper"),
+};
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/person/{name}", (string name) =>
+    people.Where(p => p.FirstName.StartsWith(name)));
 
 app.Run();
+
+
+public record Person(string FirstName, string LastName);
