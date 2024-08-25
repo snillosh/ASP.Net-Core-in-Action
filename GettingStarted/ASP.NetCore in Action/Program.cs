4d51211079a1425ab2a47fcee89e5bbfcd45dc2a
Bevan <bevansalter@googlemail.com>
2024-08-25 11:22:37 +0100

    Using StatusCodePagesMiddleware to return Problem Details

--- GettingStarted/ASP.NetCore in Action/Program.cs
+++ GettingStarted/ASP.NetCore in Action/Program.cs
@@ -1,38 +1,13 @@
-using System.Collections.Concurrent;
-
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddProblemDetails();
+
 WebApplication app = builder.Build();
 
-var _fruit = new ConcurrentDictionary<string, Fruit>();
-
-app.MapGet("/fruit", () => _fruit);
-
-app.MapGet("/fruit/{id}", (string id) =>
-    _fruit.TryGetValue(id, out var fruit)
-        ? TypedResults.Ok(fruit)
-        : Results.Problem(statusCode: 404));
-
-
-app.MapPost("/fruit/{id}", (string id, Fruit fruit) =>
-    _fruit.TryAdd(id, fruit)
-        ? TypedResults.Created($"/fruit/{id}", fruit)
-        : Results.ValidationProblem(new Dictionary<string, string[]>
-          {
-              { "id", new[] { "A fruit with this id already exists" }}
-          }));
-
-app.MapPut("/fruit/{id}", (string id, Fruit fruit) =>
+if (!app.Environment.IsDevelopment())
 {
+    app.UseExceptionHandler();
+}
 
-    _fruit[id] = fruit;
-    return Results.NoContent();
-});
-
-app.MapDelete("/fruit/{id}", (string id) =>
-{
-    _fruit.TryRemove(id, out _);
-    return Results.NoContent();
-});
+app.MapGet("/", void () => throw new Exception());
 
 app.Run();
-record Fruit(string Name, int stock);
