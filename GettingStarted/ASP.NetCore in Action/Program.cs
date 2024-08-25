bc2ae083ba2f397a85b82111b441e34de6297195
Bevan <bevansalter@googlemail.com>
2024-08-25 15:36:45 +0100

    Listing 6.1 Registering multiple endpoints with WebApplication

--- GettingStarted/ASP.NetCore in Action/Program.cs
+++ GettingStarted/ASP.NetCore in Action/Program.cs
@@ -1,63 +1,12 @@
-using System.Collections.Concurrent;
-using System.Reflection;
-
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHealthChecks();
+builder.Services.AddRazorPages();
+
 WebApplication app = builder.Build();
 
-var _fruit = new ConcurrentDictionary<string, Fruit>();
-
-RouteGroupBuilder fruitApi = app.MapGroup("/fruit");
-
-fruitApi.MapGet("/", () => _fruit);
-
-RouteGroupBuilder fruitApiWithValidation = fruitApi.MapGroup("/")
-    .AddEndpointFilter<IdValidationFilter>();
-
-fruitApiWithValidation.MapGet("/{id}", (string id) =>
-    _fruit.TryGetValue(id, out var fruit)
-        ? TypedResults.Ok(fruit)
-        : Results.Problem(statusCode: 404));
-
-fruitApiWithValidation.MapPost("/{id}", (Fruit fruit, string id) =>
-    _fruit.TryAdd(id, fruit)
-        ? TypedResults.Created($"/fruit/{id}", fruit)
-        : Results.ValidationProblem(new Dictionary<string, string[]>
-          {
-              { "id", new[] { "A fruit with this id already exists" } }
-        }));
-
-fruitApiWithValidation.MapPut("/{id}", (string id, Fruit fruit) =>
-{
-    _fruit[id] = fruit;
-    return Results.NoContent();
-});
-
-fruitApiWithValidation.MapDelete("/fruit/{id}", (string id) =>
-{
-    _fruit.TryRemove(id, out _);
-    return Results.NoContent();
-});
+app.MapGet("/test", () => "Hello world!");
+app.MapHealthChecks("/healthz");
+app.MapRazorPages();
 
 app.Run();
-
-public record Fruit(string Name, int Stock);
-
-class IdValidationFilter : IEndpointFilter
-{
-    public async ValueTask<object?> InvokeAsync(
-        EndpointFilterInvocationContext context,
-        EndpointFilterDelegate next)
-    {
-        var id = context.GetArgument<string>(0);
-        if (string.IsNullOrEmpty(id) || !id.StartsWith('f'))
-        {
-            return Results.ValidationProblem(
-                new Dictionary<string, string[]>
-                {
-                    {"id", new[]{"Invalid format. Id must start with 'f'"}}
-                });
-        }
-
-        return await next(context);
-    }
-}
