8eead2b743d97dc04006c221109b49ac9285cda6
Bevan <bevansalter@googlemail.com>
2024-08-26 13:46:26 +0100

    Listing 7.1 Binding simple values using [From] attributes

--- GettingStarted/ASP.NetCore in Action/Program.cs
+++ GettingStarted/ASP.NetCore in Action/Program.cs
@@ -1,23 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-builder.Services.Configure<RouteOptions>(o =>
-{
-    o.LowercaseUrls = true;
-    o.AppendTrailingSlash = true;
-    o.LowercaseQueryStrings = false;
-});
-
 WebApplication app = builder.Build();
 
-app.MapGet("/HealthCheck", () => Results.Ok()).WithName("healthcheck");
-app.MapGet("/{name}", (string name) => name).WithName("product");
-
-app.MapGet("/", (LinkGenerator links) =>
-new[]
-{
-    links.GetPathByName("healthcheck"),
-    links.GetPathByName("product",
-        new { Name = "Big-Widget", Q = "Test"})
-});
+app.MapGet("/products/{id}/paged",
+    ([FromRoute] int id,
+     [FromQuery] int page,
+     [FromHeader(Name = "PageSize")] int pageSize)
+     => $"Received id {id}, page {page}, pageSize {pageSize}");
 
 app.Run();
