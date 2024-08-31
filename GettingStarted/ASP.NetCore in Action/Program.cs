c97e016e1d1e374cd6305a83fafd0ec1ea48108a
Bevan <bevansalter@googlemail.com>
2024-08-31 13:25:45 +0100

    Listing 7.2 Implementing TryParse in a custom type to allow parsing from route values - Enhanced with IParsable interface

--- GettingStarted/ASP.NetCore in Action/Program.cs
+++ GettingStarted/ASP.NetCore in Action/Program.cs
@@ -1,12 +1,35 @@
-using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 
-app.MapGet("/products/{id}/paged",
-    ([FromRoute] int id,
-     [FromQuery] int page,
-     [FromHeader(Name = "PageSize")] int pageSize)
-     => $"Received id {id}, page {page}, pageSize {pageSize}");
-
+app.MapGet("/product/{id}", (ProductId id) => $"Received {id}");
 app.Run();
+
+record ProductId(int Id) : IParsable<ProductId>
+{
+    public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out ProductId result)
+    {
+        if (s is not null
+           && s.StartsWith('p')
+           && int.TryParse(
+                s.AsSpan().Slice(1),
+                out int id))
+        {
+            result = new ProductId(id);
+            return true;
+        }
+
+        result = default;
+        return false;
+    }
+
+    static ProductId IParsable<ProductId>.Parse(string s, IFormatProvider? provider)
+    {
+        _ = int.TryParse(
+                s.AsSpan().Slice(1),
+                out int id);
+
+        return new ProductId(id);
+    }
+}
